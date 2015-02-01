fa67d6e53fa6a44106dc181d492f055670f3cd41
Vignesh Muruganantham <muruvig@gmail.com>
2015-01-31 21:59:20 -0800

    almost done

--- Test.py
+++ Test.py
@@ -10,14 +10,20 @@ def main():
 	for e in data:
 		add_to_database(e)
 
-def add_to_database(device):
-	return None
-
 def test():
 	client = MongoClient()
 	db = client.sb_hack
 	collection = db.testData
-	data = ping()
-	collection.insert(data)
-
+	data = ping()[0]
+	# for e in range(10):
+	# 	m = e['macAddress']
+	# 	minfo = e['mapInfo']
+	# 	currtime = e['statistics']['currentServerTime']
+	# 	intime = e['statistics']['firstLocatedTime']
+	# 	outtime = None
+	# 	collection.update({'macAddress':m}, {'$push':{'visit_list':{'mapInfo':minfo, 'pingTime':currtime, 'entryTime':intime, 'exitTime':outtime]}, {'location_list':{'location':minfo, 'pingTime':currtime}}}})
+	# collection.update({'name':'bye'},{'$push':{'testing':data[0]['statistics']['firstLocatedTime'], 'none':None}})
+	# for e in data:
+	collection.update({'macAddress':data['macAddress']}, {'$push':{'visit_list':{'mapInfo':data['mapInfo'], 'pingTime':data['statistics']['currentServerTime'], 'entryTime':data['statistics']['firstLocatedTime'], 'exitTime':data['statistics']['currentServerTime']}, 'location_list':{'location':data['mapInfo'], 'pingTime':data['statistics']['currentServerTime']}, 'checker':'yes'}})
+	print(data['macAddress'])
 test()
