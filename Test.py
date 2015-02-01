18fe22457deb5743b9ecfd658f98a2cce33f573e
Vignesh Muruganantham <muruvig@gmail.com>
2015-02-01 05:34:07 -0800

    added support for updating & final data structure

--- Test.py
+++ Test.py
@@ -5,25 +5,12 @@ from json import *
 def main():
 	client = MongoClient()
 	db = client.sb_hack
-	collection =db.testData
+	collection = db.workingData
 	data = ping()
-	for e in data:
-		add_to_database(e)
+	for element in data:
+		if collection.find({'macAddress':element['macAddress']}).count() != 0:
+			collection.update({'macAddress':element['macAddress']}, {'$push':{'visit_list':{'mapInfo':element['mapInfo'], 'pingTime':element['statistics']['currentServerTime'], 'entryTime':element['statistics']['firstLocatedTime'], 'exitTime':element['statistics']['currentServerTime']}, 'location_list':{'mapInfo':element['mapInfo'], 'mapCoordinate':element['mapCoordinate'], 'pingTime':element['statistics']['currentServerTime']}}})
+		else:
+			collection.insert({'macAddress':element['macAddress'], 'visit_list':[{'mapInfo':element['mapInfo'], 'pingTime':element['statistics']['currentServerTime'], 'entryTime':element['statistics']['firstLocatedTime'], 'exitTime':element['statistics']['currentServerTime']}], 'location_list':[{'mapInfo':element['mapInfo'], 'mapCoordinate':element['mapCoordinate'], 'pingTime':element['statistics']['currentServerTime']}]})
 
-def test():
-	client = MongoClient()
-	db = client.sb_hack
-	collection = db.testData
-	data = ping()[0]
-	# for e in range(10):
-	# 	m = e['macAddress']
-	# 	minfo = e['mapInfo']
-	# 	currtime = e['statistics']['currentServerTime']
-	# 	intime = e['statistics']['firstLocatedTime']
-	# 	outtime = None
-	# 	collection.update({'macAddress':m}, {'$push':{'visit_list':{'mapInfo':minfo, 'pingTime':currtime, 'entryTime':intime, 'exitTime':outtime]}, {'location_list':{'location':minfo, 'pingTime':currtime}}}})
-	# collection.update({'name':'bye'},{'$push':{'testing':data[0]['statistics']['firstLocatedTime'], 'none':None}})
-	# for e in data:
-	collection.update({'macAddress':data['macAddress']}, {'$push':{'visit_list':{'mapInfo':data['mapInfo'], 'pingTime':data['statistics']['currentServerTime'], 'entryTime':data['statistics']['firstLocatedTime'], 'exitTime':data['statistics']['currentServerTime']}, 'location_list':{'location':data['mapInfo'], 'pingTime':data['statistics']['currentServerTime']}, 'checker':'yes'}})
-	print(data['macAddress'])
-test()
+main()
